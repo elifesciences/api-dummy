b7431a9aa7f708cd0c8550c1d63a0699cfabefe2
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-07-26 10:54:47 +0100

    Add articles (#7)
    * Add articles
    
    * Fix version

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,10 +9,54 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Application();
+
+$app['articles'] = function () use ($app) {
+    $finder = (new Finder())->files()->name('*.json')->in(__DIR__.'/../data/articles');
+
+    $articles = [];
+    foreach ($finder as $file) {
+        $json = json_decode($file->getContents(), true);
+        $articles[$json['versions'][0]['id']] = $json;
+    }
+
+    uasort($articles, function (array $article1, array $article2) {
+        $article1Dates = [
+            'poa' => null,
+            'vor' => null,
+        ];
+
+        $article2Dates = [
+            'poa' => null,
+            'vor' => null,
+        ];
+
+        foreach ($article1['versions'] as $version) {
+            if (null === $article1Dates[$version['status']]) {
+                $article1Dates[$version['status']] = DateTimeImmutable::createFromFormat(DATE_ATOM,
+                    $version['published']);
+            }
+        }
+
+        foreach ($article2['versions'] as $version) {
+            if (null === $article2Dates[$version['status']]) {
+                $article2Dates[$version['status']] = DateTimeImmutable::createFromFormat(DATE_ATOM,
+                    $version['published']);
+            }
+        }
+
+        $article1Date = $article1Dates['vor'] ?? $article1Dates['poa'];
+        $article2Date = $article2Dates['vor'] ?? $article2Dates['poa'];
+
+        return $article1Date <=> $article2Date;
+    });
+
+    return $articles;
+};
 
 $app['blog-articles'] = function () use ($app) {
     $finder = (new Finder())->files()->name('*.json')->in(__DIR__.'/../data/blog-articles');
@@ -109,6 +153,172 @@ $app['subjects'] = function () use ($app) {
 $app['negotiator'] = function () {
     return new Negotiator();
 };
+
+$app->get('/articles', function (Request $request) use ($app) {
+    $accepts = [
+        'application/vnd.elife.article-list+json; version=1',
+    ];
+
+    $type = $app['negotiator']->getBest($request->headers->get('Accept'), $accepts);
+
+    if (null === $type) {
+        $type = new Accept($accepts[0]);
+    }
+
+    $version = (int) $type->getParameter('version');
+    $type = $type->getType();
+
+    $articles = $app['articles'];
+
+    $page = $request->query->get('page', 1);
+    $perPage = $request->query->get('per-page', 10);
+
+    $content = [
+        'total' => count($articles),
+        'items' => [],
+    ];
+
+    if ('desc' === $request->query->get('order', 'desc')) {
+        $articles = array_reverse($articles);
+    }
+
+    if ($request->query->has('subject')) {
+        $articles = array_filter($articles, function (array $article) use ($request) : bool {
+            $latestVersion = $article['versions'][count($article['versions']) - 1];
+
+            return count(array_intersect((array) $request->query->get('subject'), $latestVersion['subjects']));
+        });
+    }
+
+    $articles = array_slice($articles, ($page * $perPage) - $perPage, $perPage);
+
+    if (0 === count($articles) && $page > 1) {
+        throw new NotFoundHttpException('No page '.$page);
+    }
+
+    foreach ($articles as $i => $article) {
+        $latestVersion = $article['versions'][count($article['versions']) - 1];
+
+        unset($latestVersion['issue']);
+        unset($latestVersion['copyright']);
+        unset($latestVersion['researchOrganisms']);
+        unset($latestVersion['keywords']);
+        unset($latestVersion['relatedArticles']);
+        unset($latestVersion['abstract']);
+        unset($latestVersion['digest']);
+
+        $content['items'][] = $latestVersion;
+    }
+
+    $headers = ['Content-Type' => sprintf('%s; version=%s', $type, $version)];
+
+    return new Response(
+        json_encode($content, JSON_PRETTY_PRINT),
+        Response::HTTP_OK,
+        $headers
+    );
+});
+
+$app->get('/articles/{number}',
+    function (Request $request, string $number) use ($app) {
+        if (false === isset($app['articles'][$number])) {
+            throw new NotFoundHttpException('Article not found');
+        };
+
+        $latestVersion = count($app['articles'][$number]['versions']);
+
+        $subRequest = Request::create('/articles/'.$number.'/versions/'.$latestVersion, 'GET', array(),
+            $request->cookies->all(), array(), $request->server->all());
+
+        return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+    }
+);
+
+$app->get('/articles/{number}/versions',
+    function (Request $request, string $number) use ($app) {
+        if (false === isset($app['articles'][$number])) {
+            throw new NotFoundHttpException('Article not found');
+        };
+
+        $article = $app['articles'][$number];
+
+        $accepts = [
+            'application/vnd.elife.article-history+json; version=1',
+        ];
+
+        $type = $app['negotiator']->getBest($request->headers->get('Accept'), $accepts);
+
+        if (null === $type) {
+            $type = new Accept($accepts[0]);
+        }
+
+        $version = (int) $type->getParameter('version');
+        $type = $type->getType();
+
+        if (!empty($article['received'])) {
+            $content = [
+                'received' => $article['received'],
+                'accepted' => $article['accepted'],
+            ];
+        }
+
+        $content['poa'] = 0;
+        $content['vor'] = 0;
+
+        foreach ($article['versions'] as $articleVersion) {
+            ++$content[$articleVersion['status']];
+        }
+
+        return new Response(
+            json_encode($content, JSON_PRETTY_PRINT),
+            Response::HTTP_OK,
+            ['Content-Type' => sprintf('%s; version=%s', $type, $version)]
+        );
+    }
+);
+
+$app->get('/articles/{number}/versions/{version}',
+    function (Request $request, string $number, int $version) use ($app) {
+        if (false === isset($app['articles'][$number])) {
+            throw new NotFoundHttpException('Article not found');
+        };
+
+        $article = $app['articles'][$number];
+
+        if (false === isset($article['versions'][$version - 1])) {
+            throw new NotFoundHttpException('Version not found');
+        };
+
+        $articleVersion = $article['versions'][$version - 1];
+
+        if ('vor' === $articleVersion['status']) {
+            $accepts = [
+                'application/vnd.elife.article-vor+json; version=1',
+            ];
+        } else {
+            $accepts = [
+                'application/vnd.elife.article-poa+json; version=1',
+            ];
+        }
+
+        unset($articleVersion['status']);
+
+        $type = $app['negotiator']->getBest($request->headers->get('Accept'), $accepts);
+
+        if (null === $type) {
+            $type = new Accept($accepts[0]);
+        }
+
+        $version = (int) $type->getParameter('version');
+        $type = $type->getType();
+
+        return new Response(
+            json_encode($articleVersion, JSON_PRETTY_PRINT),
+            Response::HTTP_OK,
+            ['Content-Type' => sprintf('%s; version=%s', $type, $version)]
+        );
+    }
+);
 
 $app->get('/blog-articles', function (Request $request) use ($app) {
     $accepts = [
@@ -507,6 +717,21 @@ $app->get('/search', function (Request $request) use ($app) {
 
     $results = [];
 
+    foreach ($app['articles'] as $result) {
+        $result = $result['versions'][count($result['versions']) - 1];
+        $result['_search'] = strtolower(json_encode($result));
+
+        unset($result['issue']);
+        unset($result['copyright']);
+        unset($result['researchOrganisms']);
+        unset($result['keywords']);
+        unset($result['relatedArticles']);
+        unset($result['abstract']);
+        unset($result['digest']);
+
+        $results[] = $result;
+    }
+
     foreach ($app['blog-articles'] as $result) {
         $result['_search'] = strtolower(json_encode($result));
         unset($result['content']);
@@ -550,6 +775,27 @@ $app->get('/search', function (Request $request) use ($app) {
     });
 
     $allTypes = [];
+    foreach (
+        [
+            'correction',
+            'editorial',
+            'feature',
+            'insight',
+            'research-advance',
+            'research-article',
+            'research-exchange',
+            'retraction',
+            'registered-report',
+            'replication-study',
+            'short-report',
+            'tools-resources',
+        ] as $articleType
+    ) {
+        $allTypes[$articleType] = count(array_filter($results, function ($result) use ($articleType) {
+            return $articleType === $result['type'];
+        }));
+    }
+
     foreach (['blog-article', 'labs-experiment', 'podcast-episode'] as $contentType) {
         $allTypes[$contentType] = count(array_filter($results, function ($result) use ($contentType) {
             return $contentType === $result['type'];
