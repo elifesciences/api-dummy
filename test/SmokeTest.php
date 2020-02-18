695420ab405c455bbc498927201902c47395e331
nlisgo <nathan@aeopus.com>
2020-02-18 18:00:30 +0000

    Just support latest version of highlight-list

--- test/SmokeTest.php
+++ test/SmokeTest.php
@@ -258,31 +258,10 @@ final class SmokeTest extends PHPUnit_Framework_TestCase
 
         foreach ((new Finder())->files()->name('*.json')->in(__DIR__.'/../data/highlights') as $file) {
             $path = '/highlights/'.$file->getBasename('.json');
-            yield "{$path} version 3" => [
+            yield $path => [
                 $this->createRequest($path),
                 'application/vnd.elife.highlight-list+json; version=3',
             ];
-            if ('announcements' !== $file->getBasename('.json')) {
-                yield "{$path} version 2" => [
-                    $this->createRequest($path, 'application/vnd.elife.highlight-list+json; version=2'),
-                    'application/vnd.elife.highlight-list+json; version=2',
-                ];
-                yield "{$path} version 1" => [
-                    $this->createRequest($path, 'application/vnd.elife.highlight-list+json; version=1'),
-                    'application/vnd.elife.highlight-list+json; version=1',
-                ];
-            } else {
-                yield "{$path} version 2" => [
-                    $this->createRequest($path, 'application/vnd.elife.highlight-list+json; version=2'),
-                    'application/problem+json',
-                    406,
-                ];
-                yield "{$path} version 1" => [
-                    $this->createRequest($path, 'application/vnd.elife.highlight-list+json; version=1'),
-                    'application/problem+json',
-                    406,
-                ];
-            }
         }
 
         yield $path = '/interviews' => [
