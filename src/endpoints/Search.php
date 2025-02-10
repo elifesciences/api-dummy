d074745d2ad0e8a66d252f8debdfe35b0f57b91a
LinaKind <l.kinduryte@elifesciences.org>
2025-02-10 11:45:59 +0000

    Create a seam to extract search endpoint to improve readability
    https://github.com/elifesciences/issues/issues/9169

---
+++ src/endpoints/Search.php
@@ -0,0 +1,11 @@
+<?php
+
+use Silex\Application;
+
+class Search
+{
+    public static function add(Application $app)
+    {
+        return $app;
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once( __DIR__.'/../src/endpoints/Search.php');
 $app = require __DIR__.'/../src/bootstrap.php';
+
+$app = Search::add($app);
 
 $app->run();
