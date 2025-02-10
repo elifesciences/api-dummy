f6e328a7cb303eb9679c406f28a2fa68dc71d8ae
David Moulton <d.moulton@elifesciences.org>
2025-02-10 11:34:21 +0000

    Increase memory limit for PHPUnit tests
    https://github.com/elifesciences/issues/issues/9169

--- project_tests.sh
+++ project_tests.sh
@@ -5,4 +5,4 @@ rm -f build/*.xml
 
 vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p src/ web/
 vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p test/
-vendor/bin/phpunit --log-junit build/phpunit.xml
+php -d memory_limit=128M vendor/bin/phpunit --log-junit build/phpunit.xml
